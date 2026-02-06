c9f235a4ac6d66ec2f323508d9a0c1906da2fa7e
Emaan Fatima <alizbat14@gmail.com>
2026-02-07 03:31:31 +0500

    Adding React Folder

---
+++ 01basicreact/src/chai.jsx
@@ -0,0 +1,11 @@
+function Chai() {
+    return (
+        <>
+            <h1>Chai aur React</h1>
+            <h3>Cahi is ready.</h3>
+            <p> Enjoy your Chai</p>
+        </>
+
+    )
+}
+export default Chai
---
+++ Custom React/react.js
@@ -0,0 +1,13 @@
+const reactElem={
+    type:"a",
+    props:{
+        href:"https://www.google.com",
+        target:'_blank'
+    },
+    children:"click me to visit Google"
+}
+
+
+
+
+const maincontainer=document.querySelector("root")
