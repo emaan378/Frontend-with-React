8a6c4fc8a943f7175c25b2d3d0fab86de94b8f9a
Emaan Fatima <alizbat14@gmail.com>
2026-02-25 22:58:24 +0500

    Add new logics

--- Custom React/react.js
+++ Custom React/react.js
@@ -1,9 +1,17 @@
 function customRender(reactElement,container){
-    const domElement=document.createElement(reactElement.type)
-    domElement.innerHTML=reactElement.children
-    domElement.setAttribute("href",reactElement.propes.href)
-    domElement.setAttribute("target",reactElement.propes.target)
-    container.appendChild(domElement)
+//     const domElement=document.createElement(reactElement.type)
+//     domElement.innerHTML=reactElement.children
+//     domElement.setAttribute("href",reactElement.props.href)
+//     domElement.setAttribute("target",reactElement.props.target)
+//     container.appendChild(domElement)
+
+const domElement=document.createElement(reactElement.type)
+domElement.innerHTML=reatElement.children
+for(const prop in reactElement.props){
+    if (prop==="children")continue
+    domElement.setAttribute(prop,reactElemet.props[prop])
+}
+container.appendChild(domElement)
 }
 const reactElem={
     type:"a",
@@ -14,5 +22,5 @@ const reactElem={
     children:"click me to visit Google"
 }
 
-const mainContainer=document.querySelector("root")
+const mainContainer=document.querySelector("#root")
 customRender(reactElem,mainContainer)
