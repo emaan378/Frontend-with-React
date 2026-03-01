cbecc2bac52de4b85e27658623a1e20e50c2e605
Emaan Fatima <alizbat14@gmail.com>
2026-03-01 15:08:55 +0500

    add new project

--- 03-Tailwind-Props/src/App.jsx
+++ 03-Tailwind-Props/src/App.jsx
@@ -1,5 +1,6 @@
 import { useState } from 'react'
 import './App.css'
+import Card from './components/Card'
 
 function App() {
   const [count, setCount] = useState(0)
@@ -7,6 +8,8 @@ function App() {
   return (
     <>
       <h1 className='bg-green-500 text-black p-4 rounded-xl mb-4'>Tailwind Test</h1>
+      <Card username="Emaan"  btnText="click me!"/>
+      <Card username="Alesh"  btnText="visit me!"/>
      
     </>
   )
