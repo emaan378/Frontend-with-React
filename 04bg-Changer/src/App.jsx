27e667b6614f1a03a0719c436e42342ea34cbed8
Emaan Fatima <alizbat14@gmail.com>
2026-03-01 16:08:06 +0500

    Add new amazing logics

--- 04bg-Changer/src/App.jsx
+++ 04bg-Changer/src/App.jsx
@@ -1,34 +1,32 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react"
+
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [color, setColor] = useState("olive")
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    
+      <div className="w-screen h-screen" style={{ backgroundColor: color }}>
+         <div className="fixed flex flex-wrap justify-center bottom-10 inset-x-0  p-4">
+           <div className="flex flex-wrap justify-center gap-3 shadow-lg bg-white px-3 py-2 rounded-lg">
+            <button className="bg-red-500 text-white px-4 py-2 rounded-lg shadow-lg" onClick={() => setColor("red")}>Red</button>
+            <button className="bg-blue-500 text-white px-4 py-2 rounded-lg shadow-lg" onClick={() => setColor("blue")}>Blue</button>
+            <button className="bg-green-500 text-white px-4 py-2 rounded-lg shadow-lg" onClick={() => setColor("green")}>Green</button>
+            <button className="bg-gray-500 text-white px-4 py-2 rounded-lg shadow-lg" onClick={() => setColor("gray")}>Gray</button>
+            <button className="bg-purple-500 text-white px-4 py-2 rounded-lg shadow-lg" onClick={() => setColor("purple")}>Purple</button>
+            <button className="bg-orange-500 text-white px-4 py-2 rounded-lg shadow-lg" onClick={() => setColor("orange")}>Orange</button>
+            <button className="bg-yellow-500 text-white px-4 py-2 rounded-lg shadow-lg" onClick={() => setColor("yellow")}>Yellow</button>
+            <button className="bg-pink-500 text-white px-4 py-2 rounded-lg shadow-lg" onClick={() => setColor("pink")}>Pink</button>
+            <button className="bg-black text-white px-4 py-2 rounded-lg shadow-lg" onClick={() => setColor("black")}>Black</button>
+            <button className="bg-white-500 text-black px-4 py-2 rounded-lg shadow-lg" onClick={() => setColor("white")}>White</button>
+            
+            
+
+           </div>
+          
+         </div>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    
   )
 }
 
