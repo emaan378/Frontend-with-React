871d1d42913f0bdb9f10420a6ae9d72456df6bc9
Emaan Fatima <alizbat14@gmail.com>
2026-03-11 22:55:29 +0500

    Add new block of code

--- 07reactRouter/src/App.jsx
+++ 07reactRouter/src/App.jsx
@@ -2,13 +2,19 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import Header from './componenets/Header/header'
+import Home from './componenets/Home/Home'
+import Footer from './componenets/Footer/Footer'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-<h1 className='bg-green-600 p-4'>React router</h1>
+    {/* <Header/> */}
+    <Home/>
+    <Footer/>
+
     </>
   )
 }
--- 07reactRouter/src/main.jsx
+++ 07reactRouter/src/main.jsx
@@ -1,10 +1,26 @@
-import { StrictMode } from 'react'
+import { Children, StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
+import { RouterProvider } from 'react-router-dom'
+import {createBrowserRouter} from 'react-router-dom'
+import Layout from './componenets/Layout'
+import Home from './componenets/Home/Home.jsx'
+import About from './componenets/About/about.jsx'  
+const router =createBrowserRouter([{
+  path:'/',
+  element:<Layout/>,
+  children:[{
+    path:"",
+    element:<Home/>
+  },{
+    path:"About",
+    element:<About/>
+  }]
+}])
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+  <RouterProvider router={router}/>
   </StrictMode>,
 )
