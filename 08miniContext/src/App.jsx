7a059a5285064397c04433dff421019601f28cf7
Emaan Fatima <alizbat14@gmail.com>
2026-03-16 06:27:03 +0500

    Adding new block of code

--- 08miniContext/src/App.jsx
+++ 08miniContext/src/App.jsx
@@ -1,8 +1,8 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from './assets/vite.svg'
-import heroImg from './assets/hero.png'
+
 import './App.css'
+import Login from './components/Login'
+import Profile from './components/Profile'
+import UserContextProvider from './Context/UserContextProvider'
 
 function App() {
  
@@ -10,6 +10,8 @@ function App() {
   return (
     <UserContextProvider>
      <h1>React with Chai</h1>
+     <Login/>
+     <Profile/>
     </UserContextProvider>
   )
 }
