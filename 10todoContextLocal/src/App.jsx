929007761d12d4317e24beb3104378271b69fc9e
Emaan Fatima <alizbat14@gmail.com>
2026-03-26 01:52:29 +0500

    Completed the project

--- 10todoContextLocal/src/App.jsx
+++ 10todoContextLocal/src/App.jsx
@@ -4,26 +4,27 @@ import { TodoProvider } from './Context'
 import TodoForm from './Components/TodoForm'
 import TodoItem from './Components/Todoitem'
 
+
 function App() {
   const [todos, setTodos] = useState([])
   const addTodo=(todo)=>{
           setTodos((prev) =>[{id: Date.now(), ...todo} ,...prev])
         }
     const updateTodo =(id,todo) =>{
-      setTodos((prev)=>{
+      setTodos((prev)=>
         prev.map((prevTodo)=>(prevTodo.id ===id ? todo:prevTodo))
-      })
+      )
     }
     const deleteTodo =(id) =>{
       setTodos((prev)=>
         prev.filter((todo) =>todo.id !== id))
       }
    const toggoleComplete =(id) =>{
-     setTodos((prev)=> prev.map((prevTodo)=> prevTodo === id? {...prevTodo,completed:!prevTodo.completed}:prevTodo))
+     setTodos((prev)=> prev.map((prevTodo)=> prevTodo.id === id? {...prevTodo,completed:!prevTodo.completed}:prevTodo))
    }
    useEffect(() => {
        const todos= JSON.parse(localStorage.getItem("todos"))
-       if (todos && todos.lenght >0){
+       if (todos && todos.length>0){
         setTodos(todos)
        }
    }, [])
@@ -38,7 +39,7 @@ function App() {
 
 
   return (
-    <TodoProvider value={{todos,addTodo,updateTodo,deleteTodo,togoleComplete}}>
+    <TodoProvider value={{todos,addTodo,updateTodo,deleteTodo,toggoleComplete}}>
       <div className="bg-[#172842] min-h-screen py-8">
         <div className="w-full max-w-2xl mx-auto shadow-md rounded-lg px-4 py-3 text-white">
           <h1 className="text-2xl font-bold text-center mb-8 mt-2">Manage Your Todos</h1>
